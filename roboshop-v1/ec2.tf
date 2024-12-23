983892b43444129198c9a33250839862994f2617
meghlamadan <Nikhil@190112>
2024-12-23 18:15:31 +0000

    Terraform code

---
+++ roboshop-v1/ec2.tf
@@ -0,0 +1,27 @@
+resource "aws_instance" "frontend" {
+  ami           = "ami-09c813fb71547fc4f"
+  instance_type = "t3.small"
+  vpc_security_group_ids = ["sg-0bbb58be30fbb31f2"]
+
+  tags = {
+    Name = "frontend"
+  }
+}
+resource "aws_instance" "mongo" {
+  ami           = "ami-09c813fb71547fc4f"
+  instance_type = "t3.small"
+  vpc_security_group_ids = ["sg-0bbb58be30fbb31f2"]
+
+  tags = {
+    Name = "mongo"
+  }
+}
+resource "aws_instance" "catalogue" {
+  ami           = "ami-09c813fb71547fc4f"
+  instance_type = "t3.small"
+  vpc_security_group_ids = ["sg-0bbb58be30fbb31f2"]
+
+  tags = {
+    Name = "catalogue"
+  }
+}
