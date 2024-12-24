107d5cb6d64cd4454089a47bfad2095835200a42
meghlamadan <meghlamadan@gmail.com>
2024-12-24 00:14:17 +0000

    Terraform code ec1

--- roboshop-v1/ec1.tf
+++ roboshop-v1/ec1.tf
@@ -7,6 +7,8 @@ resource "aws_instance" "frontend" {
     Name = "frontend"
   }
 }
+
+
 resource "aws_instance" "mongo" {
   ami           = "ami-09c813fb71547fc4f"
   instance_type = "t3.small"
@@ -16,13 +18,12 @@ resource "aws_instance" "mongo" {
     Name = "mongo"
   }
 }
-
 resource "aws_instance" "Redis" {
   ami           = "ami-09c813fb71547fc4f"
   instance_type = "t3.small"
   vpc_security_group_ids = ["sg-0bbb58be30fbb31f2"]
 
   tags = {
-    Name = "catalogue"
+    Name = "Redis"
   }
 }
